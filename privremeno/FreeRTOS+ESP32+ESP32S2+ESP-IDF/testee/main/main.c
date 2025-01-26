e1230bb23c43b8e5f74f09afdb1365d2e24c45ba
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2025-01-26 21:54:58 +0100

    dodano opterecenje

--- privremeno/FreeRTOS+ESP32+ESP32S2+ESP-IDF/testee/main/main.c
+++ privremeno/FreeRTOS+ESP32+ESP32S2+ESP-IDF/testee/main/main.c
@@ -11,7 +11,7 @@
  * - RESPONSE_PIN = output (default GPIO18)
  * - LED_PIN      = output -- samo radi testiranja (default GPIO19)
  *   ako je samo jedan sklop, može se spojiti LED_PIN => REQUEST_PIN
- *   i testirati kod
+ *   i testirati kod (ali ovo nije testirano!)
  */
 
 #include <stdio.h>
@@ -24,11 +24,16 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "esp_timer.h"
+#include "esp_task_wdt.h"
+#include "esp_heap_caps.h"
+#include "esp_random.h"
+#include "bootloader_random.h"
+#include "esp_chip_info.h"
 
 /* Načini rada */
 #define TEST_MODE_INTERRUPT  1
-#define TEST_MODE_THREAD     2
-#define TEST_MODE            TEST_MODE_INTERRUPT
+#define TEST_MODE_TASK     2
+#define TEST_MODE            TEST_MODE_TASK
 
 static const char *TAG = "testee";
 
@@ -43,7 +48,7 @@ static const char *TAG = "testee";
 #define HIGH 1
 #define LOW  0
 
-#define BACKGROUND_TASKS  0
+#define BACKGROUND_TASKS  4
 #define WD_TIMEOUT	500000 //500 ms - da dretva bar jednom u tom intervalu spava
 			//da neki watchdog timer ne prekine tu dretvu
 			//možda postaviti na veću vrijednost?
@@ -52,18 +57,23 @@ static const char *TAG = "testee";
 #if TEST_MODE == TEST_MODE_INTERRUPT
 //kad dođe zahtjev onda se:
 //1. šalje odgovor - preko drugog pina (odmah, bez "odlaganja")
-//2. postavlja timer da za 10 ms ugasi odgovor za zahtjev (spisti stanje pina)
+//2. postavlja timer da za 10 ms ugasi odgovor za zahtjev (spusti stanje pina)
 
-TimerHandle_t xTimer;
+static TimerHandle_t xTimer;
 
+//obrada prekid (kad na REQUEST_PIN dođe zahtjev)
 static void IRAM_ATTR handleRequestInterrupt(void* arg)
 {
-	gpio_set_level(RESPONSE_PIN, HIGH); //odgovori na zahtjev
+	//odgovori na zahtjev
+	gpio_set_level(RESPONSE_PIN, HIGH);
 
+	//postavi alarm za reset odgovora
 	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 	xTimerStartFromISR(xTimer, &xHigherPriorityTaskWoken);
 	portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
+
+//funkcija za aktiviranje alarma
 static void resetResponsePinTimer(TimerHandle_t xTimer)
 {
 	gpio_set_level(RESPONSE_PIN, LOW); //ugasi odgovor
@@ -81,9 +91,13 @@ static void setup_interrupt_mode()
 	io_conf.pull_down_en = 0;
 	gpio_config(&io_conf);
 
+	//pokreni sustav za prihvat prekida
 	gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+
+	//registriraj prekidnu funkciju za prekid na pinu
 	gpio_isr_handler_add(REQUEST_PIN, handleRequestInterrupt, NULL);
 
+	//stvori alarm (ali neaktivan)
 	xTimer = xTimerCreate("Timer", pdMS_TO_TICKS(10), pdFALSE,
 			(void *) 0, resetResponsePinTimer);
 }
@@ -91,10 +105,11 @@ static void setup_interrupt_mode()
 ///////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////
-#if TEST_MODE == TEST_MODE_THREAD
-TaskHandle_t RequestProcessingTaskHandle;
+#if TEST_MODE == TEST_MODE_TASK
+static TaskHandle_t RequestProcessingTaskHandle;
 
-void RequestProcessingTask(void *pvParameters)
+//dretva za reakciju na događaj
+static void RequestProcessingTask(void *pvParameters)
 {
 	int64_t last = esp_timer_get_time();
 
@@ -121,25 +136,29 @@ void RequestProcessingTask(void *pvParameters)
 		}
 	}
 }
-void setup_thread_mode()
+
+static void setup_thread_mode()
 {
 	//postavi pin za ulaz
 	gpio_config_t io_conf = {};
 
-	io_conf.intr_type = GPIO_INTR_DISABLE;
+	io_conf.intr_type = GPIO_INTR_DISABLE; //ne prekidi
 	io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
 	io_conf.mode = GPIO_MODE_INPUT;
 	io_conf.pull_up_en = 1;
 	io_conf.pull_down_en = 0;
 	gpio_config(&io_conf);
 
-	xTaskCreate(RequestProcessingTask, "Handle Request", 2048, NULL,
-		configMAX_PRIORITIES - 1, &RequestProcessingTaskHandle);
+	xTaskCreate(RequestProcessingTask, "Handle Request", 4096, NULL,
+		configMAX_PRIORITIES - 2, &RequestProcessingTaskHandle);
 }
-
-#endif //TEST_MODE_THREAD
+#endif //TEST_MODE_TASK
 ///////////////////////////////////////////////////////////////////////////////
+#if BACKGROUND_TASKS > 0
+	static void setup_background_tasks(); //samo deklaracija, kod je ispod
+#endif
 
+//početna dretva
 void app_main(void)
 {
 	ESP_LOGI(TAG, "Inicijalizacija");
@@ -156,17 +175,111 @@ void app_main(void)
 
 	gpio_set_level(RESPONSE_PIN, LOW);
 
-#if TEST_MODE == TEST_MODE_INTERRUPT
-	setup_interrupt_mode();
-#elif TEST_MODE == TEST_MODE_THREAD
-	setup_thread_mode();
-#else
-	#error TEST_MODE nije TEST_MODE_INTERRUPT ni TEST_MODE_THREAD
-#endif
+	//ovoj dretvi najveći prioritet, da joj ostale ne smetaju dok postavlja sve
+	vTaskPrioritySet(NULL, configMAX_PRIORITIES - 1);
 
 #if BACKGROUND_TASKS > 0
 	setup_background_tasks();
 #endif
 
+#if TEST_MODE == TEST_MODE_INTERRUPT
+	setup_interrupt_mode();
+#elif TEST_MODE == TEST_MODE_TASK
+	setup_thread_mode();
+#else
+	#error TEST_MODE nije TEST_MODE_INTERRUPT ni TEST_MODE_TASK
+#endif
+
 	ESP_LOGI(TAG, "Inicijalizacija gotova, spreman za zahtjeve");
 }
+
+///////////////////////////////////////////////////////////////////////////////
+#if BACKGROUND_TASKS > 0
+static TaskHandle_t backgroundTaskHandle[BACKGROUND_TASKS];
+#define TPERIOD  100000 //100 ms
+static int64_t period = 1000;
+static int *bgdata = NULL;
+static size_t bgdata_size = 0;
+static uint8_t cores = 2;
+
+//unutarnja petlja koje izvode dretve u pozadini
+static void background_loop()
+{
+	for (int64_t i = 0; i < period; i++) {
+		if (bgdata) {
+			//simulirati neki rad
+			uint32_t r1 = esp_random() % bgdata_size;
+			uint32_t r2 = esp_random() % bgdata_size;
+			bgdata[r1] = bgdata[r1] + bgdata[r2] + r1 + r2;
+		}
+		else {
+			asm volatile("":::"memory");
+		}
+	}
+}
+
+//pozadinska dretva
+static void backgroundTask(void *pvParameters)
+{
+	long id = (long) pvParameters;
+
+	long inner_loops = id * 1000000 / TPERIOD;
+	if (BACKGROUND_TASKS > 1)
+		inner_loops = inner_loops * cores / BACKGROUND_TASKS;
+	//Ideja: otprilike svakih 'id' sekundi po jedan ispis za dretvu 'id'
+	//Unutarnja petlja traje oko TPERIOD (kada dretva radi na procesoru),
+	//pa bi (1000000 / TPERIOD) unutarnjih petlji bila sekunda.
+	//Ali obzirom da dretve dijele vrijeme svaka će dobiti "dio"
+	//procesorskog vremena, pa je 10 iteracija petlje više od sekunde.
+	//Zato je broj petlji skaliran s brojem jezgri i brojem dretvi.
+
+	ESP_LOGI(TAG, "Krece dretva %ld", id);
+
+	for (;;) {
+		for (long i = 0; i < inner_loops; i++) {
+			background_loop();
+			taskYIELD();
+		}
+		ESP_LOGI(TAG, "Dretva %ld nesto radi", id);
+	}
+}
+
+//izračunaj 'period' za unutarnju petlju
+static void set_period()
+{
+	int64_t t0 = 0, t1 = 0;
+
+	while (t1 - t0 < TPERIOD) {
+		period *= 2;
+		t0 = esp_timer_get_time();
+		background_loop();
+		t1 = esp_timer_get_time();
+	}
+	//skaliraj period da bude TPERIOD usec rada
+	double p = ((double) period) * TPERIOD / (t1 - t0);
+	period = (int64_t) p; //double radi mogućeg preljeva
+}
+
+static void setup_background_tasks()
+{
+	esp_chip_info_t chip_info;
+	esp_chip_info(&chip_info);
+	cores = chip_info.cores;
+
+	esp_task_wdt_deinit(); //da wdt ne prekida sustav
+
+	bgdata_size = heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT) / 2 / sizeof(int);
+	bgdata = heap_caps_calloc(sizeof(int), bgdata_size, MALLOC_CAP_DEFAULT);
+	if (bgdata == NULL)
+		ESP_LOGI(TAG, "Nema memorije za bgdata");
+
+	bootloader_random_enable();
+
+	set_period();
+
+	for (int i = 0; i < BACKGROUND_TASKS; i++)
+		xTaskCreate(backgroundTask, "Background Task", 4096,
+			(void *) (i+1), 1, &backgroundTaskHandle[i]);
+}
+#endif //BACKGROUND_TASKS > 0
+///////////////////////////////////////////////////////////////////////////////
--- privremeno/FreeRTOS+ESP32+ESP32S2+ESP-IDF/tester/main/main.c
+++ privremeno/FreeRTOS+ESP32+ESP32S2+ESP-IDF/tester/main/main.c
@@ -2,14 +2,14 @@
  * Program za testiranje
  *
  * Testira se vrijeme odziva na vanjski zahtjev
- * => ovaj sustav šalje zahjtve i mjeri vrijeme odziva
+ * => ovaj sustav šalje zahtjeve i mjeri vrijeme odziva
  *
  * koristi se ESP32S2 (ESP32-S2 chip (via ESP-PROG), UART za Flash Method)
  *
  * GPIO pinovi: (postavljeni u Kconfig.projbuild; tamo ih i mijenjati po potrebi)
  * - REQUEST_PIN  = output (default GPIO7)
  * - RESPONSE_PIN = input, pulled up, interrupt from rising edge (default GPIO8)
- * - LED_PIN      = output -- samo radi testiranja (default GPIO9)
+ * - LED_PIN      = output -- samo radi testiranja (default GPIO9) (ali ovo nije testirano!)
  */
 
 #include <stdio.h>
@@ -39,29 +39,32 @@ static const char *TAG = "tester";
 /* Načini rada */
 #define TEST_MODE_INTERRUPT  1
 #define TEST_MODE_THREAD     2
-#define TEST_MODE            TEST_MODE_INTERRUPT
+#define TEST_MODE            TEST_MODE_TASK
 
 #define TEST_PERIOD_MS  100  //svako koliko provoditi slanje zahtjeva
 #define TESTS           50   //koliko ukupno testova napraviti
 
-int64_t t_req[TESTS], t_resp[TESTS];
-int test = 0;
+static int64_t t_req[TESTS], t_resp[TESTS]; //vremena zahtjeva i odziva
+static int test = 0; //redni broj testa
 
-TaskHandle_t ResponseTestTaskHandle;
+static TaskHandle_t ResponseTestTaskHandle;
 
+//dretva za testiranje
 static void ResponseTestTask(void *pvParameters)
 {
-	vTaskDelay(pdMS_TO_TICKS(1000)); //možda malo pričekati prije testova
+	vTaskDelay(pdMS_TO_TICKS(1000)); //malo pričekati prije testova
 
 	for (; test < TESTS; test++)
 	{
 		//ESP_LOGI(TAG, "saljem");
 		t_resp[test] = 0;
-		t_req[test] = esp_timer_get_time();
+		t_req[test] = esp_timer_get_time(); //zabilježi vrijeme zahtjeva
+		//ako se uzme poslije gpio_set, onda je vrijeme kraće za ~1usec
+
 		gpio_set_level(REQUEST_PIN, HIGH); //generiranje zahtjeva
 		//t_req[test] = esp_timer_get_time();
 
-		#if TEST_MODE == TEST_MODE_THREAD
+#if TEST_MODE == TEST_MODE_TASK
 		int timeout = 0;
 
 		//ESP_LOGI(TAG, "Cekam");
@@ -79,7 +82,7 @@ static void ResponseTestTask(void *pvParameters)
 			t_resp[test] = t;
 
 		gpio_set_level(REQUEST_PIN, LOW);
-		#endif //TEST_MODE_THREAD
+#endif //TEST_MODE_TASK
 
 		vTaskDelay(pdMS_TO_TICKS(TEST_PERIOD_MS)); //vrijeme mirovanja (perioda)
 	}
@@ -89,44 +92,45 @@ static void ResponseTestTask(void *pvParameters)
 	#endif //TEST_MODE_INTERRUPT
 
 	ESP_LOGI(TAG, "Testiranje gotovo, izracunavam statistiku");
-	//izračunaj statistiku
-	int odgovora = 0;
-	int64_t max_obrada = 0, min_obrada = TEST_PERIOD_MS;
-	double obrada = 0.0;
+	int responses = 0;
+	int64_t max_response = 0, min_response = TEST_PERIOD_MS;
+	double avg_response = 0.0;
 
 	for (int i = 0; i < TESTS; i++)
 	{
 		if (t_resp[i] != 0) {
 			int64_t t = t_resp[i] - t_req[i];
-			obrada += t;
-			if (t > max_obrada)
-				max_obrada = t;
-			if (t < min_obrada)
-				min_obrada = t;
-			odgovora++;
+			avg_response += t;
+			if (t > max_response)
+				max_response = t;
+			if (t < min_response)
+				min_response = t;
+			responses++;
 			ESP_LOGI(TAG, "odgovor[%d]: %lld", i, t);
 			vTaskDelay(1);//inače se ruši kad koristim prekide!?
 		}
 	}
 
-	if (odgovora > 0)
-		obrada = obrada / odgovora;
+	if (responses > 0)
+		avg_response = avg_response / responses;
 
-	ESP_LOGI(TAG, "avg: %g", obrada);
-	ESP_LOGI(TAG, "min: %lld", min_obrada);
-	ESP_LOGI(TAG, "max: %lld", max_obrada);
-	ESP_LOGI(TAG, "timeouts: %d", TESTS - odgovora);
+	ESP_LOGI(TAG, "avg: %g", avg_response);
+	ESP_LOGI(TAG, "min: %lld", min_response);
+	ESP_LOGI(TAG, "max: %lld", max_response);
+	ESP_LOGI(TAG, "timeouts: %d", TESTS - responses);
 
 	vTaskSuspend(NULL);
 }
 
 
 #if TEST_MODE == TEST_MODE_INTERRUPT
+//obrada prekid (kad na RESPONSE_PIN dođe odgovor)
 static void IRAM_ATTR irqResponseArrived(void* arg)
 {
-	t_resp[test] = esp_timer_get_time();
+	t_resp[test] = esp_timer_get_time(); //zabilježi vrijeme odgovora
 	gpio_set_level(REQUEST_PIN, LOW); //ugasi zahtjev
 }
+
 static void setup_interrupt_mode()
 {
 	//postavi pin za ulaz
@@ -139,7 +143,10 @@ static void setup_interrupt_mode()
 	io_conf.pull_down_en = 0;
 	gpio_config(&io_conf);
 
+	//pokreni sustav za prihvat prekida
 	gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+
+	//registriraj prekidnu funkciju za prekid na pinu
 	gpio_isr_handler_add(RESPONSE_PIN, irqResponseArrived, NULL);
 
 	//zahtjev šalje dretva
@@ -150,13 +157,13 @@ static void setup_interrupt_mode()
 ///////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////
-#if TEST_MODE == TEST_MODE_THREAD
-void setup_thread_mode()
+#if TEST_MODE == TEST_MODE_TASK
+static void setup_thread_mode()
 {
 	//postavi pin za ulaz
 	gpio_config_t io_conf = {};
 
-	io_conf.intr_type = GPIO_INTR_DISABLE;
+	io_conf.intr_type = GPIO_INTR_DISABLE; //ne prekidi
 	io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
 	io_conf.mode = GPIO_MODE_INPUT;
 	io_conf.pull_up_en = 1;
@@ -167,9 +174,10 @@ void setup_thread_mode()
 		configMAX_PRIORITIES - 1, &ResponseTestTaskHandle);
 }
 
-#endif //TEST_MODE_THREAD
+#endif //TEST_MODE_TASK
 ///////////////////////////////////////////////////////////////////////////////
 
+//početna dretva
 void app_main(void)
 {
 	ESP_LOGI(TAG, "Inicijalizacija");
@@ -188,10 +196,10 @@ void app_main(void)
 
 #if TEST_MODE == TEST_MODE_INTERRUPT
 	setup_interrupt_mode();
-#elif TEST_MODE == TEST_MODE_THREAD
+#elif TEST_MODE == TEST_MODE_TASK
 	setup_thread_mode();
 #else
-	#error TEST_MODE nije TEST_MODE_INTERRUPT ni TEST_MODE_THREAD
+	#error TEST_MODE nije TEST_MODE_INTERRUPT ni TEST_MODE_TASK
 #endif
 
 	ESP_LOGI(TAG, "Inicijalizacija gotova, pokrecem testiranje");
